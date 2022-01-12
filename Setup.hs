0d7ca8317967e5f402a21823f474aff4539de70a
Koz Ross <koz.ross@runbox.com>
2022-01-12 21:33:56 +1300

    Enable CI on Windows (and macOS) (#6)
    * Try enabling macOS CI
    
    * Try enabling Windows build
    
    * Explain what the hell is going on in Setup.hs
    
    * Avoid unnecessary rebuilds on POSIX platforms

--- Setup.hs
+++ Setup.hs
@@ -6,13 +6,45 @@ import Distribution.System (OS(..), buildOS)
 import Debug.Trace
 import Distribution.Types.LocalBuildInfo
 import System.Process (system)
+import System.FilePath ((</>), (<.>))
+import System.Directory (copyFile, doesFileExist, withCurrentDirectory)
 
 main = defaultMainWithHooks $
   simpleUserHooks
     { postConf = \_args _configFlags _packageDescription localBuildInfo -> do
+        -- Cabal, and indeed, GHC, don't understand the .lib extension on
+        -- Windows, so we have the same name everywhere.
+        let destinationPath = traceId $ buildDir localBuildInfo </> "libsodium" <.> "a"
         case buildOS of
-          Windows -> error "Build is not supported on Windows yet."
-          _ ->
-            let destinationPath = traceId $ buildDir localBuildInfo <> "/libsodium.a"
-             in void $ system $ "cd cbits/libsodium-stable/ && ./configure && make -j && cp -v ./src/libsodium/.libs/libsodium.a " <> destinationPath
+          Windows -> do
+            -- We're in a bit of a bind when it comes to Windows. The chief
+            -- problem is that the _only_ shell we have access to is CMD.EXE:
+            -- this means that, even though we _could_ have Autotools access in
+            -- theory (since GHC needs MinGW, which comes with the Autotools),
+            -- we can't use them. Furthermore, we can't be clever and do a
+            -- Visual Studio build, for two reasons:
+            --
+            -- 1. It would require our users to have Visual Studio installed,
+            --    which is quite onerous.
+            -- 2. We would have to detect where Visual Studio put the compiler,
+            --    then drive a Visual Studio build, from the command line,
+            --    _manually_. This is even _more_ onerous!
+            --
+            -- Thus, we use a bundled static prebuild. This is not ideal, as it
+            -- bloats the distribution, but there's very little we can do about
+            -- this.
+            copyFile ("winlibs" </> "libsodium" <.> "lib") destinationPath
+          _ -> do
+            -- Since everything else is some flavour of POSIX, we can use the
+            -- Autotools to build in-place. This current (more involved) setup
+            -- avoids triggering unnecessary rebuilds by checking if configure
+            -- and/or make already ran.
+            let workPath = "cbits" </> "libsodium-stable"
+            let sourcePath = workPath </> "src" </> "libsodium" </> ".libs" </> "libsodium" <.> "a"
+            let makefilePath = workPath </> "Makefile"
+            configureRan <- doesFileExist makefilePath
+            makeRan <- doesFileExist sourcePath
+            unless configureRan (withCurrentDirectory workPath (void . system $ "./configure"))
+            unless makeRan (withCurrentDirectory workPath (void . system $ "make -j"))
+            copyFile sourcePath destinationPath
     }

546f45d37a456dde4d795e365ca815bb6280a00a
Koz Ross <koz.ross@retro-freedom.nz>
2022-01-12 13:39:40 +1300

    Try enabling Windows build

--- Setup.hs
+++ Setup.hs
@@ -6,13 +6,16 @@ import Distribution.System (OS(..), buildOS)
 import Debug.Trace
 import Distribution.Types.LocalBuildInfo
 import System.Process (system)
+import System.FilePath ((</>), (<.>))
+import System.Directory (copyFile)
 
 main = defaultMainWithHooks $
   simpleUserHooks
     { postConf = \_args _configFlags _packageDescription localBuildInfo -> do
+        let destinationPath = traceId $ buildDir localBuildInfo </> "libsodium" <.> "a"
         case buildOS of
-          Windows -> error "Build is not supported on Windows yet."
-          _ ->
-            let destinationPath = traceId $ buildDir localBuildInfo <> "/libsodium.a"
-             in void $ system $ "cd cbits/libsodium-stable/ && ./configure && make -j && cp -v ./src/libsodium/.libs/libsodium.a " <> destinationPath
+          Windows -> do
+            copyFile ("winlibs" </> "libsodium" <.> "lib") destinationPath
+          _ -> do
+            void $ system $ "cd cbits/libsodium-stable/ && ./configure && make -j && cp -v ./src/libsodium/.libs/libsodium.a " <> destinationPath
     }

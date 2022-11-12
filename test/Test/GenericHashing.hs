a9557b2f192e09ee379748b32128a1c55b360d85
Hécate Moonlight <hecate+github@glitchbra.in>
2022-11-12 22:18:48 +0100

    Fix tests

--- test/Test/GenericHashing.hs
+++ test/Test/GenericHashing.hs
@@ -4,29 +4,25 @@ module Test.GenericHashing where
 
 import Test.Tasty
 import Test.Tasty.HUnit
-import qualified Data.ByteString as BS
 import Foreign hiding (void)
 import Control.Monad (void)
 import Foreign.C
-import LibSodium.Bindings.GenericHashing (cryptoGenericHash, cryptoGenericHashBytes)
+import qualified Data.ByteString.Internal as BS
 import Data.ByteString (ByteString)
-
-useByteString :: ByteString -> ((Ptr a, CSize) -> IO b) -> IO b
-useByteString bs f =
-  BS.useAsCStringLen bs $ \(b, l) ->
-    f (castPtr b, fromIntegral l)
+import LibSodium.Bindings.GenericHashing (cryptoGenericHash, cryptoGenericHashBytes, cryptoGenericHashKeyBytes)
 
 spec :: TestTree
 spec =
   testGroup
     "Generic hashing tests"
-    [ testCase "cryptoGenericHash" testCryptoGenericHash
+    [ testCase "cryptoGenericHash without key" testCryptoGenericHashWithoutKey
+    ,  testCase "cryptoGenericHash with key" testCryptoGenericHashWithKey
     ]
 
-testCryptoGenericHash :: Assertion
-testCryptoGenericHash =
+testCryptoGenericHashWithoutKey :: Assertion
+testCryptoGenericHashWithoutKey =
   withCStringLen "test test" $ \(cString, cstringLength) ->
-  allocaBytes (sizeOf (undefined :: CUChar)) $ \outPtr -> do
+  allocaBytes (fromIntegral cryptoGenericHashBytes) $ \outPtr -> do
     void $ cryptoGenericHash
       outPtr
       cryptoGenericHashBytes
@@ -35,6 +31,30 @@ testCryptoGenericHash =
       nullPtr
       0
     out <- peekCString (castPtr outPtr)
-    assertEqual "Hashed test string is consistent with key"
-                "\DEL=\ETB\SOp\ETB\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL o\1186$\DEL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL"
+    assertEqual "Hashed test string is consistent without key"
+                "\DEL=\ETB\SOp\ETBd=\STXBLzL\SOHl2\CAN\DELQ"
                 out
+
+testCryptoGenericHashWithKey :: Assertion
+testCryptoGenericHashWithKey =
+  let key = "af7d1575690407317bf93723a8d1dca5"
+      msg = "Test Test" :: String
+      keyLength = cryptoGenericHashKeyBytes
+  in
+    withCStringLen msg $ \(cString, cstringLength) ->
+    allocaBytes (fromIntegral cryptoGenericHashBytes) $ \outPtr ->
+    withByteString key $ \keyPtr -> do
+      void $ cryptoGenericHash
+        outPtr
+        cryptoGenericHashBytes
+        (castPtr cString :: Ptr CUChar)
+        (fromIntegral cstringLength)
+        keyPtr
+        keyLength
+      out <- peekCString (castPtr outPtr)
+      assertEqual "Hashed test string is consistent without key"
+                  "<|1e$\SO3\888\GSsrs\15158\ESC4"
+                  out
+
+withByteString :: ByteString -> (Ptr a -> IO b) -> IO b
+withByteString (BS.PS fptr off _) f = withForeignPtr fptr $ \ptr -> f $! (ptr `plusPtr` off)

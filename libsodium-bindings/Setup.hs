9cfe42802b8aa54af50b41984dc2e096a7e4e3d3
Hécate Moonlight <hecate+github@glitchbra.in>
2023-12-11 15:20:19 +0100

    :pig:

--- libsodium-bindings/Setup.hs
+++ libsodium-bindings/Setup.hs
@@ -7,29 +7,66 @@ import Distribution.Simple.Setup
 import Distribution.System
 import Distribution.Types.Flag
 import Distribution.Types.LocalBuildInfo
-import System.Directory (copyFile, doesFileExist, renameFile, withCurrentDirectory)
+import System.Directory (copyFile, doesFileExist, renameFile, withCurrentDirectory, getCurrentDirectory, createDirectoryIfMissing)
 import System.FilePath ((<.>), (</>))
+import Data.Maybe
 import System.Process (system)
+import qualified Distribution.PackageDescription as PD
 
 main =
   defaultMainWithHooks $
     simpleUserHooks
-      { postConf = \_args configFlags _packageDescription localBuildInfo ->
-          case lookupFlagAssignment (mkFlagName "bundled-libsodium") (configConfigurationsFlags configFlags) of
-            Just True -> do
-              putStrLn "Building with the bundled libsodium 1.0.18-stable"
-              -- Cabal, and indeed, GHC, don't understand the .lib extension on
-              -- Windows, so we have the same name everywhere.
-              let destinationPath = traceId $ buildDir localBuildInfo </> "libCsodium" <.> "a"
-              case (buildOS, buildArch) of
-                (Windows, X86_64) -> do
-                  copyFile ("binaries" </> "winlibs" </> "libsodium" <.> "a") destinationPath
-                (Linux, X86_64) -> do
-                  copyFile ("binaries" </> "x86_64-unknown-linux" </> "libsodium" <.> "a") destinationPath
-                (FreeBSD, X86_64) -> do
-                  copyFile ("binaries" </> "x86_64-unknown-freebsd" </> "libsodium" <.> "a") destinationPath
-                (os, arch) ->
-                  putStrLn $ "Static libsodium builds for " <> show arch <> "-unknown-" <> show os <> " are not supported yet."
-            _ ->
-              putStrLn "Building with the system-wide libsodium"
+      { confHook = customConfHook
+      , postConf = customPostConfHook
       }
+
+customConfHook
+  :: (PD.GenericPackageDescription, PD.HookedBuildInfo)
+  -> ConfigFlags
+  -> IO LocalBuildInfo
+customConfHook (description, buildInfo) flags = do
+  localBuildInfo <- confHook simpleUserHooks (description, buildInfo) flags
+  let packageDescription = localPkgDescr localBuildInfo
+      library = fromJust $ PD.library packageDescription
+      libraryBuildInfo = PD.libBuildInfo library
+  dir <- getCurrentDirectory
+  pure
+    localBuildInfo
+      { localPkgDescr =
+          packageDescription
+            { PD.library =
+                Just $
+                  library
+                    { PD.libBuildInfo =
+                        libraryBuildInfo
+                          { PD.extraLibDirs =
+                              (dir </> ".libsodium" </> show buildOS </> show buildArch)
+                                : PD.extraLibDirs libraryBuildInfo
+                          }
+                    }
+            }
+      }
+
+customPostConfHook :: Args -> ConfigFlags -> p2 -> LocalBuildInfo -> IO ()
+customPostConfHook _args configFlags _packageDescription localBuildInfo =
+  case lookupFlagAssignment (mkFlagName "bundled-libsodium") (configConfigurationsFlags configFlags) of
+    Just True -> do
+      putStrLn "Building with the bundled libsodium 1.0.18-stable"
+      dir <- getCurrentDirectory
+      let destinationPath = traceId $ buildDir localBuildInfo </> "libCsodium" <.> "a"
+      let wellKnownPath = traceId $ dir </> ".libsodium" </> show buildOS <> "-" <> show buildArch
+      case (buildOS, buildArch) of
+        (Windows, X86_64) -> do
+          -- Cabal, and indeed, GHC, don't understand the .lib extension on
+          -- Windows, so we have the same name everywhere.
+          copyFile ("binaries" </> "x86_64-windows" </> "libsodium" <.> "a") destinationPath
+        (Linux, X86_64) -> do
+          copyFile ("binaries" </> "x86_64-linux" </> "libsodium" <.> "a") destinationPath
+          createDirectoryIfMissing True wellKnownPath
+          copyFile ("binaries" </> "x86_64-linux" </> "libsodium" <.> "a") (wellKnownPath </> "libCsodium" <.> "a")
+        (FreeBSD, X86_64) -> do
+          copyFile ("binaries" </> "x86_64-freebsd" </> "libsodium" <.> "a") destinationPath
+        (os, arch) ->
+          putStrLn $ "Static libsodium builds for " <> show arch <> "-" <> show os <> " are not supported yet."
+    _ ->
+      putStrLn "Building with the system-wide libsodium"

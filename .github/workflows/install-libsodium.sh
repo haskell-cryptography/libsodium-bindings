5e83c46c40e76f1dfdadd74f9f0a40f2bbbbca10
Hécate <Kleidukos@users.noreply.github.com>
2024-06-16 12:47:51 +0200

    Use pkg-config in CI (#152)

--- .github/workflows/install-libsodium.sh
+++ .github/workflows/install-libsodium.sh
@@ -2,6 +2,6 @@
 
 case "$(uname -s)" in
         Linux*) sudo apt install libsodium-dev;;
-        Darwin*) brew install libsodium;;
+        Darwin*) brew reinstall libsodium && brew install llvm;;
 esac
 

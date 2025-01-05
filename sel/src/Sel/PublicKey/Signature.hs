9a08f39b9d80f4d45eb622e24267f873d2aba06c
Jack Henahan <root@proofte.ch>
2025-01-05 18:09:52 -0500

    feat(Signature): add serde functions, hide internals
    Public API
    ----------
    
    - Adds `KeyMaterialDecodeError` to signal issues during
      deserialization
    
    - Adds serde functions for public and secret keys
    
    - Adds a function to extract the public key from a secret key, with
      `PublicKeyExtraction` on failure
    
    - Adds `Show` and `Display` instances for `PublicKey`, `SecretKey`, and `SignedMessage`
    
    - Re-exports `PublicKey`, `SecretKey`, and `SignedMessage` from the internal module, but
      no other visible change
    
    - Expands documentation
    
    Internals
    ---------
    
    - Moves signature internals to an internal module using the new
      `Scoped` machinery; this is only _really_ required because we need
      to get at the raw pointers to do anything interesting, but the guts
      don't need to be on display for the public API; if this is
      disagreeable, it can be moved wholesale into the public module
    
    - Adds a (presently only internal) mechanism for tracking verification
      of signatures; this is intended to become public as part of an RFC
      on the API, but remains internal until something comes of that
    
    - Adds `KeyPair`, just a name for the existing `(PublicKey,
      SecretKey)` representation; this is also intended to become public as
      part of the RFC

--- sel/src/Sel/PublicKey/Signature.hs
+++ sel/src/Sel/PublicKey/Signature.hs
@@ -1,240 +1,148 @@
-{-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications #-}
-
 -- |
 --
 -- Module: Sel.PublicKey.Signature
 -- Description: Public-key signatures with the Ed25519 algorithm
--- Copyright: (C) Hécate Moonlight 2022
+-- Copyright: (C) Hécate Moonlight 2022, Jack Henahan 2024
 -- License: BSD-3-Clause
 -- Maintainer: The Haskell Cryptography Group
 -- Portability: GHC only
 module Sel.PublicKey.Signature
-  ( -- ** Introduction
+  ( -- * Public-key Signatures
     -- $introduction
+
+    -- ** Public keys
+    -- $publicKeys
     PublicKey
+  , decodePublicKeyHexByteString
+  , encodePublicKeyHexByteString
+
+    -- ** Secret keys
+    -- $secretKeys
   , SecretKey
-  , SignedMessage
+  , decodeSecretKeyHexByteString
+  , publicKey
+
+    -- *** ⚠️ Handle with care
+  , unsafeEncodeSecretKeyHexByteString
 
     -- ** Key Pair generation
   , generateKeyPair
 
     -- ** Message Signing
+  , SignedMessage
   , signMessage
-  , openMessage
 
-    -- ** Constructing and Deconstructing
+    -- *** Inspecting signed messages
+  , openMessage
   , getSignature
   , unsafeGetMessage
+
+    -- *** Detached signatures
   , mkSignature
+
+    -- ** Exceptions
+  , PublicKeyExtractionException (..)
   )
 where
 
-import Control.Monad (void)
 import Data.ByteString (StrictByteString)
-import Data.ByteString.Unsafe (unsafePackMallocCStringLen)
-import qualified Data.ByteString.Unsafe as ByteString
-import Foreign
-  ( ForeignPtr
-  , Ptr
-  , castPtr
-  , mallocBytes
-  , mallocForeignPtrBytes
-  , withForeignPtr
-  )
-import Foreign.C (CChar, CSize, CUChar, CULLong)
-import qualified Foreign.Marshal.Array as Foreign
-import qualified Foreign.Ptr as Foreign
-import GHC.IO.Handle.Text (memcpy)
+import Sel.Internal.Scoped (use)
+import Sel.PublicKey.Internal.Signature
 import System.IO.Unsafe (unsafeDupablePerformIO)
-
-import LibSodium.Bindings.CryptoSign
-  ( cryptoSignBytes
-  , cryptoSignDetached
-  , cryptoSignKeyPair
-  , cryptoSignPublicKeyBytes
-  , cryptoSignSecretKeyBytes
-  , cryptoSignVerifyDetached
-  )
-import Sel.Internal
 
 -- $introduction
 --
--- Public-key Signatures work with a 'SecretKey' and 'PublicKey'
+-- Append a signature to any number of messages using a
+-- t'SecretKey'. Distribute a t'PublicKey' so third-parties can verify
+-- that the messages were signed with a particular t'SecretKey'.
 --
--- * The 'SecretKey' is used to append a signature to any number of messages. It must stay private;
--- * The 'PublicKey' is used by third-parties to to verify that the signature appended to a message was
--- issued by the creator of the public key. It must be distributed to third-parties.
+-- * The t'SecretKey' must stay private.
 --
--- Verifiers need to already know and ultimately trust a public key before messages signed
--- using it can be verified.
+-- * The t'PublicKey' is not a proof of identity, only control. Ensure
+-- that t'PublicKey's are trusted before verifying signatures.
 
--- |
+-- $publicKeys
 --
--- @since 0.0.1.0
-newtype PublicKey = PublicKey (ForeignPtr CUChar)
+-- Public keys are intended to be shared with any party or process
+-- which may need to verify that a given message was signed by a
+-- particular secret key.
+--
+-- === Human-readable output
+--
+-- * @'Data.Text.Display.display' :: t'PublicKey' -> 'Data.Text.Text'@, the hexadecimal encoding of the t'PublicKey' in a 'Data.Text.Text'
+-- * @'show' :: t'PublicKey' -> 'String'@, the hexadecimal encoding of the t'PublicKey' in a 'String'
 
--- |
+-- $secretKeys
 --
--- @since 0.0.1.0
-instance Eq PublicKey where
-  (PublicKey pk1) == (PublicKey pk2) =
-    foreignPtrEq pk1 pk2 cryptoSignPublicKeyBytes
-
--- |
+-- Secret keys are intended to be private and never shared without
+-- extreme care. Leaking a secret key allows anyone to impersonate the
+-- creator of that key and sign messages with their identity.
 --
--- @since 0.0.1.0
-instance Ord PublicKey where
-  compare (PublicKey pk1) (PublicKey pk2) =
-    foreignPtrOrd pk1 pk2 cryptoSignPublicKeyBytes
-
--- |
+-- If a secret key is compromised, all messages signed by that key
+-- should be considered compromised.
 --
--- @since 0.0.1.0
-newtype SecretKey = SecretKey (ForeignPtr CUChar)
-
--- |
+-- Secret keys are compared for equality using
+-- 'LibSodium.Bindings.Comparison.sodiumMemcmp' and lexicographically
+-- using 'LibSodium.Bindings.Comparison.sodiumCompare', both
+-- constant-time comparisons, to guard against timing attacks.
 --
--- @since 0.0.1.0
-instance Eq SecretKey where
-  (SecretKey sk1) == (SecretKey sk2) =
-    foreignPtrEqConstantTime sk1 sk2 cryptoSignSecretKeyBytes
-
--- |
+-- === ⚠️ Serialization
 --
--- @since 0.0.1.0
-instance Ord SecretKey where
-  compare (SecretKey sk1) (SecretKey sk2) =
-    foreignPtrOrd sk1 sk2 cryptoSignSecretKeyBytes
-
--- |
---
--- @since 0.0.1.0
-data SignedMessage = SignedMessage
-  { messageLength :: CSize
-  , messageForeignPtr :: ForeignPtr CUChar
-  , signatureForeignPtr :: ForeignPtr CUChar
-  }
-
--- |
---
--- @since 0.0.1.0
-instance Eq SignedMessage where
-  (SignedMessage len1 msg1 sig1) == (SignedMessage len2 msg2 sig2) =
-    let
-      messageLength = len1 == len2
-      msg1Eq = foreignPtrEq msg1 msg2 len1
-      msg2Eq = foreignPtrEq sig1 sig2 cryptoSignBytes
-     in
-      messageLength && msg1Eq && msg2Eq
-
--- |
---
--- @since 0.0.1.0
-instance Ord SignedMessage where
-  compare (SignedMessage len1 msg1 sig1) (SignedMessage len2 msg2 sig2) =
-    let
-      messageLength = compare len1 len2
-      msg1Ord = foreignPtrOrd msg1 msg2 len1
-      msg2Ord = foreignPtrOrd sig1 sig2 cryptoSignBytes
-     in
-      messageLength <> msg1Ord <> msg2Ord
+-- * @'unsafeEncodeSecretKeyHexByteString' :: t'SecretKey' -> 'StrictBytestring'@
 
 -- | Generate a pair of public and secret key.
 --
--- The length parameters used are 'cryptoSignPublicKeyBytes'
--- and 'cryptoSignSecretKeyBytes'.
+-- The length parameters used are 'LibSodium.Bindings.CryptoSign.cryptoSignPublicKeyBytes'
+-- and 'LibSodium.Bindings.CryptoSign.cryptoSignSecretKeyBytes'.
 --
 -- @since 0.0.1.0
 generateKeyPair :: IO (PublicKey, SecretKey)
-generateKeyPair = do
-  publicKeyForeignPtr <- mallocForeignPtrBytes (fromIntegral @CSize @Int cryptoSignPublicKeyBytes)
-  secretKeyForeignPtr <- mallocForeignPtrBytes (fromIntegral @CSize @Int cryptoSignSecretKeyBytes)
-  withForeignPtr publicKeyForeignPtr $ \pkPtr ->
-    withForeignPtr secretKeyForeignPtr $ \skPtr ->
-      void $
-        cryptoSignKeyPair
-          pkPtr
-          skPtr
-  pure (PublicKey publicKeyForeignPtr, SecretKey secretKeyForeignPtr)
+generateKeyPair = (,) <$> public <*> secret <$> keyPair
 
--- | Sign a message.
+-- | Sign a message with a t'SecretKey'.
+--
+-- === Example
+--
+-- Given @keys :: 'Traversable' t => t t'SecretKey'@ and @message ::
+-- 'StrictByteString'@, we can sign our message with each key.
+--
+-- @
+--   traverse (signMessage message) keys -- :: Traversable t => IO (t 'SignedMessage')
+--   -- or, equivalently
+--   for keys (signMessage message)
+-- @
 --
 -- @since 0.0.1.0
 signMessage :: StrictByteString -> SecretKey -> IO SignedMessage
-signMessage message (SecretKey skFPtr) =
-  ByteString.unsafeUseAsCStringLen message $ \(cString, messageLength) -> do
-    let sigLength = fromIntegral @CSize @Int cryptoSignBytes
-    (messageForeignPtr :: ForeignPtr CUChar) <- Foreign.mallocForeignPtrBytes messageLength
-    signatureForeignPtr <- Foreign.mallocForeignPtrBytes sigLength
-    withForeignPtr messageForeignPtr $ \messagePtr ->
-      withForeignPtr signatureForeignPtr $ \signaturePtr ->
-        withForeignPtr skFPtr $ \skPtr -> do
-          Foreign.copyArray messagePtr (Foreign.castPtr @CChar @CUChar cString) messageLength
-          void $
-            cryptoSignDetached
-              signaturePtr
-              Foreign.nullPtr -- Always of size 'cryptoSignBytes'
-              (castPtr @CChar @CUChar cString)
-              (fromIntegral @Int @CULLong messageLength)
-              skPtr
-    pure $ SignedMessage (fromIntegral @Int @CSize messageLength) messageForeignPtr signatureForeignPtr
+signMessage message secretKey = use $ sign secretKey message
 
--- | Open a signed message with the signatory's public key.
--- The function returns 'Nothing' if there is a key mismatch.
+-- | Attempt to extract the message from a t'SignedMessage', verifying
+-- that the message was signed with the t'SecretKey' corresponding to
+-- the given t'PublicKey'.
 --
 -- @since 0.0.1.0
 openMessage :: SignedMessage -> PublicKey -> Maybe StrictByteString
-openMessage SignedMessage{messageLength, messageForeignPtr, signatureForeignPtr} (PublicKey pkForeignPtr) = unsafeDupablePerformIO $
-  withForeignPtr pkForeignPtr $ \publicKeyPtr ->
-    withForeignPtr signatureForeignPtr $ \signaturePtr -> do
-      withForeignPtr messageForeignPtr $ \messagePtr -> do
-        result <-
-          cryptoSignVerifyDetached
-            signaturePtr
-            messagePtr
-            (fromIntegral @CSize @CULLong messageLength)
-            publicKeyPtr
-        case result of
-          (-1) -> pure Nothing
-          _ -> do
-            bsPtr <- mallocBytes (fromIntegral messageLength)
-            memcpy bsPtr (castPtr messagePtr) messageLength
-            Just <$> unsafePackMallocCStringLen (castPtr bsPtr :: Ptr CChar, fromIntegral messageLength)
+openMessage message key =
+  case unsafeDupablePerformIO . use $ open message key of
+    Valid msg -> Just msg
+    Invalid -> Nothing
 
--- | Get the signature part of a 'SignedMessage'.
+-- | Get the signature part of a t'SignedMessage'.
 --
 -- @since 0.0.1.0
 getSignature :: SignedMessage -> StrictByteString
-getSignature SignedMessage{signatureForeignPtr} = unsafeDupablePerformIO $
-  withForeignPtr signatureForeignPtr $ \signaturePtr -> do
-    bsPtr <- Foreign.mallocBytes (fromIntegral cryptoSignBytes)
-    memcpy bsPtr signaturePtr cryptoSignBytes
-    unsafePackMallocCStringLen (Foreign.castPtr bsPtr :: Ptr CChar, fromIntegral cryptoSignBytes)
+getSignature = unsafeDupablePerformIO . use . extractSignature
 
--- | Get the message part of a 'SignedMessage' __without verifying the signature__.
+-- | Get the message part of a t'SignedMessage' __without verifying the signature__.
 --
 -- @since 0.0.1.0
 unsafeGetMessage :: SignedMessage -> StrictByteString
-unsafeGetMessage SignedMessage{messageLength, messageForeignPtr} = unsafeDupablePerformIO $
-  withForeignPtr messageForeignPtr $ \messagePtr -> do
-    bsPtr <- Foreign.mallocBytes (fromIntegral messageLength)
-    memcpy bsPtr messagePtr messageLength
-    unsafePackMallocCStringLen (Foreign.castPtr bsPtr :: Ptr CChar, fromIntegral messageLength)
+unsafeGetMessage = unsafeDupablePerformIO . use . extractUnverifiedMessage
 
--- | Combine a message and a signature into a 'SignedMessage'.
+-- | Construct a signed message from a message and a detached signature.
 --
 -- @since 0.0.1.0
 mkSignature :: StrictByteString -> StrictByteString -> SignedMessage
-mkSignature message signature = unsafeDupablePerformIO $
-  ByteString.unsafeUseAsCStringLen message $ \(messageStringPtr, messageLength) ->
-    ByteString.unsafeUseAsCStringLen signature $ \(signatureStringPtr, _) -> do
-      (messageForeignPtr :: ForeignPtr CUChar) <- Foreign.mallocForeignPtrBytes messageLength
-      signatureForeignPtr <- Foreign.mallocForeignPtrBytes (fromIntegral cryptoSignBytes)
-      withForeignPtr messageForeignPtr $ \messagePtr ->
-        withForeignPtr signatureForeignPtr $ \signaturePtr -> do
-          Foreign.copyArray messagePtr (Foreign.castPtr messageStringPtr) messageLength
-          Foreign.copyArray signaturePtr (Foreign.castPtr signatureStringPtr) (fromIntegral cryptoSignBytes)
-      pure $ SignedMessage (fromIntegral @Int @CSize messageLength) messageForeignPtr signatureForeignPtr
+mkSignature messageBytes signatureBytes =
+  unsafeDupablePerformIO . use $
+    buildSignedMessage messageBytes signatureBytes
--- sel/test/Test/PublicKey/Signature.hs
+++ sel/test/Test/PublicKey/Signature.hs
@@ -5,20 +5,84 @@ module Test.PublicKey.Signature where
 import Sel.PublicKey.Signature
 import Test.Tasty
 import Test.Tasty.HUnit
+import TestUtils
 
 spec :: TestTree
-spec =
-  testGroup
-    "Signing tests"
-    [ testCase "Sign a message with a public key and decrypt it with a secret key" testSignMessage
-    ]
+spec = withKeyPair $ \kp ->
+  testGroup "Signature" $
+    sequence [serdes, signing] kp
 
-testSignMessage :: Assertion
-testSignMessage = do
-  (publicKey, secretKey) <- generateKeyPair
-  signedMessage <- signMessage "hello hello" secretKey
-  let result = openMessage signedMessage publicKey
+serdes :: IO (PublicKey, SecretKey) -> TestTree
+serdes kp =
+  testGroup "Key pair serdes" $
+    keyPairCases
+      kp
+      [ ("Public key round-trip", publicKeyRoundTrip)
+      , ("Secret key round-trip", secretKeyRoundTrip)
+      , ("Public key extraction", publicKeyExtraction)
+      ]
+
+signing :: IO (PublicKey, SecretKey) -> TestTree
+signing kp =
+  testGroup "Message signing" $
+    keyPairCases
+      kp
+      [ ("Sign and open with the same key", signAndOpenSelf)
+      , ("Sign and open with another key", signAndOpenOther)
+      , ("Detached signature round-trip", signRoundTrip)
+      ]
+
+keyPairCases :: IO (PublicKey, SecretKey) -> [(String, (PublicKey, SecretKey) -> Assertion)] -> [TestTree]
+keyPairCases = fmap . uncurry . usingKeyPair
+
+withKeyPair :: (IO (PublicKey, SecretKey) -> TestTree) -> TestTree
+withKeyPair = withResource generateKeyPair mempty
+
+usingKeyPair :: IO (PublicKey, SecretKey) -> String -> ((PublicKey, SecretKey) -> Assertion) -> TestTree
+usingKeyPair kp testName test = testCase testName (test =<< kp)
+
+publicKeyRoundTrip :: (PublicKey, SecretKey) -> Assertion
+publicKeyRoundTrip (public, _) = do
+  let encoded = encodePublicKeyHexByteString public
+  decoded <- assertRight $ decodePublicKeyHexByteString encoded
+  assertEqual "Public key hex decode" public decoded
+
+secretKeyRoundTrip :: (PublicKey, SecretKey) -> Assertion
+secretKeyRoundTrip (_, secret) = do
+  let encoded = unsafeEncodeSecretKeyHexByteString secret
+  decoded <- assertRight $ decodeSecretKeyHexByteString encoded
+  assertEqual "Secret key hex decode" secret decoded
+
+publicKeyExtraction :: (PublicKey, SecretKey) -> Assertion
+publicKeyExtraction (public, secret) =
+  assertEqual "Public key extraction" public (publicKey secret)
+
+signAndOpenSelf :: (PublicKey, SecretKey) -> Assertion
+signAndOpenSelf (public, secret) = do
+  let message = "SIGNED"
+  signed <- signMessage message secret
+  let result = openMessage signed public
   assertEqual
-    "Message is well-opened with the correct key"
-    (Just "hello hello")
+    "Open self-signed message"
+    (Just message)
     result
+
+signAndOpenOther :: (PublicKey, SecretKey) -> Assertion
+signAndOpenOther (_, secret) = do
+  let message = "SIGNED"
+  (otherPublic, _) <- generateKeyPair
+  signed <- signMessage message secret
+  let result = openMessage signed otherPublic
+  assertEqual
+    "Fail to open with another key"
+    Nothing
+    result
+
+signRoundTrip :: (PublicKey, SecretKey) -> Assertion
+signRoundTrip (_, secret) = do
+  let message = "SIGNED"
+  signed <- signMessage message secret
+  let unverified = unsafeGetMessage signed
+      detachedSignature = getSignature signed
+      reconstructed = mkSignature unverified detachedSignature
+  assertEqual "Round trip" signed reconstructed
